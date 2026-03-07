d401d295a2fa92e0faee43b834772bd87a1f0fcd
Lakshay Narula <lakshayn02@gmail.com>
2026-03-07 22:05:10 +0530

    feat: Update CORS origin to allow Vercel deployment URL instead of localhost.

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['https://zomato-sable-delta.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
